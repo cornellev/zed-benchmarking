994c2c840f83d3b4139e50cfbca3f5f99feb30ae
ChickenCoupe <cornellev@cornell.edu>
2024-02-03 16:21:54 -0500

    Fix bugs with camera processor node

--- src/zed-camera/src/camera_processor.py
+++ src/zed-camera/src/camera_processor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 
 from nav_msgs.msg import Odometry
@@ -9,7 +11,7 @@ class CameraProcessor:
     
     def __init__(self):
         rospy.Subscriber("/zed/zed_node/odom", Odometry, self.read_odom)
-        self.odom_pub = rospy.Publisher("camera_processor/odom", data_class=Odometry)
+        self.odom_pub = rospy.Publisher("camera_processor/odom", data_class=Odometry, queue_size=10)
 
     def read_odom(self, odom):
         self.odom_pub.publish(odom)
