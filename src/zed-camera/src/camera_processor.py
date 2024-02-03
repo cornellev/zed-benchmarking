d689f46b56996d0e68a25a87b4ce6d9dfa4d5832
Myles Pasetsky <myles.pasetsky@gmail.com>
2024-02-03 16:13:42 -0500

    Add test custom zed package

---
+++ src/zed-camera/src/camera_processor.py
@@ -0,0 +1,23 @@
+import rospy
+
+from nav_msgs.msg import Odometry
+
+"""
+Simple (re)publisher of the ZED odometry, for testing purposes
+"""
+class CameraProcessor:
+    
+    def __init__(self):
+        rospy.Subscriber("/zed/zed_node/odom", Odometry, self.read_odom)
+        self.odom_pub = rospy.Publisher("camera_processor/odom", data_class=Odometry)
+
+    def read_odom(self, odom):
+        self.odom_pub.publish(odom)
+
+
+if __name__ == "__main__":
+    rospy.init_node("camera_processor")
+
+    cp = CameraProcessor()
+
+    rospy.spin()
